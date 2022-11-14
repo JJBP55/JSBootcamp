e73bd114a47c6132146fc268d445cfecfddb3105
JJBP55 <jonathanjbello.jb@gmail.com>
2022-11-13 21:50:20 -0400

    Arrow Functions Ex3

---
+++ module1/exercises2/arrowFunctions/ex3.js
@@ -0,0 +1,7 @@
+//Crea una Arrow Function que calcule el área de un rectángulo y la muestre por consola.
+
+const areaRectangulo = (h, b) => b * h;
+
+areaRectangulo();
+
+console.log(areaRectangulo(6,4));

c6d1759bdd2c0539dd4e77a65795ef13a61dc43b
JJBP55 <jonathanjbello.jb@gmail.com>
2022-11-23 19:39:25 -0400

    Arrays Exercise  3

--- module1/exercises3/ex3.js
+++ module1/exercises3/ex3.js
@@ -4,3 +4,29 @@
 // Un palíndromo es una palabra o frase que se lee igual de izquierda a derecha que de derecha a izquierda.
 
 //  Ahora haz la misma comprobación pero con un array de strings.
+
+function palindromo(palabra){
+
+    const reves = palabra.split("").reverse().join("");
+
+    if (reves === palabra){
+        console.log(`${palabra} es palíndromo`)
+    }
+    else{
+        console.log(`${palabra} no es palíndromo`)
+    }
+    return;
+};
+
+palindromo('reconocer');
+
+
+// Segundo metodo con funciones flechas
+
+const voltear = (palabra) => palabra.split("").reverse().join("");
+
+const palindromo2 = (palabra) => {
+    (palabra === voltear(palabra)) ? console.log("La palabra es palindroma") : console.log("La palabra no es palindroma"); 
+};
+
+palindromo2("reconocer");

4b9fb0f1808589b339cdbbe45e634542bafce4b3
JJBP55 <jonathanjbello.jb@gmail.com>
2022-11-24 18:52:35 -0400

    Arrays Exercise 5

--- module1/exercises3/ex4.js
+++ module1/exercises3/ex4.js
@@ -2,3 +2,11 @@
 
 // Crea una función que reciba un array de nombres que le han dado like a una foto, e indica por consola quien ha dado like.
 
+
+function likes(array){
+    array.forEach((element) => {
+        console.log(`${element}`); 
+    });
+}
+
+likes();
--- module1/exercises3/ex5.js
+++ module1/exercises3/ex5.js
@@ -13,3 +13,65 @@
 //  Haz la suma de todas las edades de las personas.
 
 //  Crea un nuevo array solo con la ciudad de cada persona y su primer hobby. 
+
+const users = [
+    {
+      name: 'Juan',
+      age: 25,
+      city: 'Madrid',
+      hobbies: ['fútbol', 'poker', 'programar'],
+    },
+    {
+      name: 'María',
+      age: 30,
+      city: 'Barcelona',
+      hobbies: ['pintar', 'leer', 'programar'],
+    },
+    {
+      name: 'Pedro',
+      age: 20,
+      city: 'Madrid',
+      hobbies: ['fútbol', 'programar'],
+    },
+    {
+      name: 'Laura',
+      age: 35,
+      city: 'Barcelona',
+      hobbies: ['pintar', 'leer', 'programar'],
+    },
+    {
+      name: 'Pablo',
+      age: 27,
+      city: 'Madrid',
+      hobbies: ['fútbol', 'poker', 'programar'],
+    },
+];
+
+//Madrileños
+const madrilenos = users.filter((user) => user.city === 'Madrid');
+
+
+console.log("Madrileños: ");
+madrilenos.forEach((user) => console.log(user.name));
+
+
+//Mayores de 25 años
+const mayores = users.filter((user) => user.age >= 25);
+
+console.log("\nMayores de 25 años: ");
+mayores.forEach((user) => console.log(user.name));
+
+//Mayores de 25 y Madrileño
+const madrilenosMayores = users.filter((user) => user.city === 'Madrid' && user.age >= 25);
+
+console.log("\nMadrileños mayores de 25 años: ");
+madrilenosMayores.forEach((user) => console.log(user.name));
+
+//Primera persona que le guste programar
+const program = users.find((user) => user.hobbies.forEach('programar'));
+
+console.log(program);
+
+
+//ciudad de cada persona y su primer hobby. 
+
